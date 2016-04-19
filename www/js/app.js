6e216201019d3eaf9948cb1df51f427624bf9edf
Kristian Kunde <kunde.kristian@gmail.com>
2016-04-19 14:37:46 +0200

    add a config angular block to set the Local Storage prefix
    It is a good practice to add a prefix to our stored entities so as avoid being overwritten in future. Add a config angular block to set the Local Storage prefix

--- www/js/app.js
+++ www/js/app.js
@@ -1,2 +1,7 @@
 //setup angular
 var app = angular.module('scotch-todo', ['ionic', 'LocalStorageModule']);
+
+app.config(function (localStorageServiceProvider) {
+    localStorageServiceProvider
+        .setPrefix('scotch-todo');
+});
